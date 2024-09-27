a5c0097fc0969cb46e23126ef9eb48b9fa017719
Yozabel Hamuda <yozabelhamuda@gmail.com>
2024-09-27 09:42:34 +0700

    Change on Controller

--- app/Http/Controllers/SSOController.php
+++ app/Http/Controllers/SSOController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class EmployeeController extends Controller
+class SSOController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SSOController;
 
-Route::get('/', [EmployeeController::class, 'index'])->name('home');
-Route::match(['get', 'post'], '/sso-process', [EmployeeController::class, 'ssoprocess'])->name('sso.process');
+Route::get('/', [SSOController::class, 'index'])->name('home');
+Route::match(['get', 'post'], '/sso-process', [SSOController::class, 'ssoprocess'])->name('sso.process');
 Route::get('/alphabit', [EmployeeController::class, 'alphabit'])->name('alphabit');
 Route::get('/icore', [EmployeeController::class, 'icore'])->name('icore');
 Route::get('/other-app', [EmployeeController::class, 'otherApp'])->name('other.app');
